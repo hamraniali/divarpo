17bacfc444cf66e1ca7dcfbcb743d20f0b52cd0a
alihamrani <alihamrani@localhost.localdomain>
2019-08-17 09:44:38 -0400

    main page for mobile completed

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -42,10 +42,20 @@
 <body>
     <header class="mdc-top-app-bar my-font" dir="ltr" style="background-color: white;box-shadow: 3px 3px 3px #e0e0e0">
         <div class="mdc-top-app-bar__row" dir="rtl">
-                    <button class="material-icons mdc-icon-button bold-font menu-btn" data-mdc-ripple-is-unbounded="true" style="color: black;margin-top: 7px;margin-right: 15px;font">menu</button>
-                    <span class="mdc-top-app-bar__title my-font" style="line-height: 63px;padding-left: 23px;">
-                    <a href="#"><img class="logo" src="{{ asset('/images/download.png') }}" alt="my site" style="margin-top: 14px"></a>
+                    <div class="desk-head">
+                        <button class="material-icons mdc-icon-button bold-font menu-btn" data-mdc-ripple-is-unbounded="true" style="color: black;margin-top: 7px;margin-right: 15px;">menu</button>
+                        <span class="mdc-top-app-bar__title my-font" style="line-height: 63px;padding-left: 23px;">
+                    <a href="#"><img class="logo" src="{{ asset('/images/download.png') }}" alt="my site" style="margin-top: -18px"></a>
                 </span>
+                    </div>
+
+            <div class="mobile-head">
+                <button class="material-icons mdc-icon-button bold-font menu-btn-mobile" data-mdc-ripple-is-unbounded="true" style="color: black;margin-top: 7px;margin-right: 15px;    position: absolute;
+    right: 0;float: right">menu</button>
+                <span class="mdc-top-app-bar__title my-font" style="line-height: 63px;padding-left: 23px;">
+                    <a href="#"><img class="logo" src="{{ asset('/images/download.png') }}" alt="my site" style="margin-top: -7px"></a>
+                </span>
+            </div>
             <section class="mdc-top-app-bar__section section-search" style="padding-right: 30px;">
                 <div style="width: 100%;height: 47px;border-radius: 150rem;display: inherit;padding-left: 10px;">
                     <input type="text" class="box-search" placeholder="جستوجو...">
@@ -75,11 +85,12 @@
                 </div>
             </section>
 
-            <a href="#" class="my-font blue-color-back btn-ripple mdc-ripple-surface btn-sabt my-font bold-font" style="float: left">ثبت رایگان آگهی</a>
-
+            <a href="#!" class="my-font blue-color-back btn-ripple mdc-ripple-surface btn-sabt my-font bold-font desk-add" style="float: left">ثبت رایگان آگهی</a>
         </div>
     </header>
-
+    <button class="mdc-fab mdc-fab blue-color-back floating-action-btn">
+        <span class="material-icons mdc-fab__icon">add</span>
+    </button>
     <aside class="mdc-drawer mdc-drawer--modal" dir="rtl" style="box-shadow: none !important;border-right:none;border-top:none;border-bottom:none ">
         <div class="mdc-drawer__header">
             <button class="material-icons mdc-icon-button bold-font menu-btn-close"
@@ -117,19 +128,23 @@
         <div class="container">
             <div class="box-filter-search">
                 <input placeholder="جستوجو..." type="text" style="border-radius: 5px;width: 100%;height: 45px;background-color: white;border: 1px solid #e0e0e0;padding: 10px;direction: rtl;">
-                <div style="display: flow-root;margin-top: 10px;width: 100%">
-                    <select name="filter" id="fliter" class="filter-select" style="border-radius: 5px !important;float: right!important;margin: 0 !important;">
-                        <option value="" selected>شهر</option>
-                        <option value="">شهر</option>
-                        <option value="">شهر</option>
-                        <option value="">شهر</option>
-                    </select>
-                    <select name="filter" id="fliter" class="filter-select" style="border-radius: 5px !important;float: left!important;">
-                        <option value="" selected>دسته</option>
-                        <option value="">دسته</option>
-                        <option value="">دسته</option>
-                        <option value="">دسته</option>
-                    </select>
+                <div style="display: flex;margin-top: 10px;width: 100%">
+                    <div style="width: 50%">
+                        <select name="filter" id="fliter" class="filter-select" style="width: 100%;border-radius: 5px !important;float: right!important;margin: 0 !important;">
+                            <option value="" selected>شهر</option>
+                            <option value="">شهر</option>
+                            <option value="">شهر</option>
+                            <option value="">شهر</option>
+                        </select>
+                    </div>
+                    <div style="width: 50%">
+                        <select name="filter" id="fliter" class="filter-select" style="width: 100%;border-radius: 5px !important;float: left!important;">
+                            <option value="" selected>دسته</option>
+                            <option value="">دسته</option>
+                            <option value="">دسته</option>
+                            <option value="">دسته</option>
+                        </select>
+                    </div>
                 </div>
                 <div style="width: 100%;height: 40px;margin-top: 10px;">
                     <button style="width: 100%;height: 100%;color: white;font-size: 16px;border: none;border-radius: 150rem" class="blue-color-back my-font btn-ripple mdc-ripple-surface">ثبت فیلتر</button>
@@ -164,7 +179,7 @@
                 <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                     <div class="card" style="margin-top: 10px">
                         <div class="card-body box-agahi box-agahi-title">
-                            <a href="#">
+                            <a href="#!">
                                 <div style="width: 100%;height: 150px;display: inline-flex;background-color: #9e9e9e">
                                     <div style="width: 50%;height: 100%;border-bottom: solid 1px #e0e0e0;background-image: url('https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/2.jpg')"></div>
                                     <div style="width: 50%;height: 100%;background-color: white;border-bottom: 1px #e0e0e0 solid;padding: 7px;text-align: right;direction: rtl">
@@ -207,12 +222,16 @@
 <script type="text/javascript">
     drawer = mdc.drawer.MDCDrawer.attachTo(document.querySelector(".mdc-drawer"));
     const listEl = document.querySelector('.menu-btn');
+    const mobEl = document.querySelector('.menu-btn-mobile');
     const close = document.querySelector('.menu-btn-close');
     const mainContentEl = document.querySelector('.main-content');
 
-        listEl.addEventListener('click', (event) => {
+    listEl.addEventListener('click', (event) => {
             drawer.open = true;
-        });
+    });
+    mobEl.addEventListener('click', (event) => {
+        drawer.open = true;
+    });
     close.addEventListener('click', (event) => {
         drawer.open = false;
     });
